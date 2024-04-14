e1eeafaf76beb1bdf6a6139ab9deab21f1910ecb
mainman22 <arundhathiramajyam@gmail.com>
2024-04-14 17:01:01 +0530

    Half Chapter 12

--- Chap11Pygame/Chap11PygameProject.py
+++ Chap11Pygame/Chap11PygameProject.py
@@ -1,27 +1,34 @@
 import pygame
 from settings import Settings
 from ship import Ship
+from alien import Alien
 import game_functions as gf
 from pygame.sprite import Group
 
 
 def run_game():
+
     pygame.init()
 
     ai_settings = Settings()
-    screen = pygame.display.set_mode(
-        (ai_settings.screen_width, ai_settings.screen_height))
-    ship = Ship(screen, ai_settings)
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    ship = Ship(screen, ai_settings)
+
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, aliens, ship)
 
     while True:
 
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)  # After ship update before update screen
+        gf.update_bullets(bullets, aliens)  # After ship update before update screen
         # Updates position of bullets ^^^
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings, screen, ship, bullets, aliens)
 
 
 run_game()
